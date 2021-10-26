4eec8afb2b030f7dbfe3fd46b72a44d59f3a83d5
lokehsienjie <loke.hj@u.nus.edu>
2021-10-26 16:34:49 +0800

    Partner Entity added

---
+++ HotelReservationSystemLibrary/src/entity/Partner.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import util.enumeration.PartnerAccessRight;
+
+/**
+ *
+ * @author hsien
+ */
+@Entity
+public class Partner implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long partnerId;
+    private String organisation;
+    private PartnerAccessRight accessRight;
+    private String username;
+    private String password;
+
+    public Partner() {
+    }
+
+    public Partner(String organisation, PartnerAccessRight accessRight, String username, String password) {
+        this.organisation = organisation;
+        this.accessRight = accessRight;
+        this.username = username;
+        this.password = password;
+    }
+
+    
+
+    public Long getPartnerId() {
+        return partnerId;
+    }
+
+    public void setPartnerId(Long partnerId) {
+        this.partnerId = partnerId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (partnerId != null ? partnerId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Partner)) {
+            return false;
+        }
+        Partner other = (Partner) object;
+        if ((this.partnerId == null && other.partnerId != null) || (this.partnerId != null && !this.partnerId.equals(other.partnerId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Partner[ id=" + partnerId + " ]";
+    }
+
+    /**
+     * @return the organisation
+     */
+    public String getOrganisation() {
+        return organisation;
+    }
+
+    /**
+     * @param organisation the organisation to set
+     */
+    public void setOrganisation(String organisation) {
+        this.organisation = organisation;
+    }
+
+    /**
+     * @return the accessRight
+     */
+    public PartnerAccessRight getAccessRight() {
+        return accessRight;
+    }
+
+    /**
+     * @param accessRight the accessRight to set
+     */
+    public void setAccessRight(PartnerAccessRight accessRight) {
+        this.accessRight = accessRight;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
---
+++ HotelReservationSystemLibrary/src/util/enumeration/PartnerAccessRight.java
@@ -0,0 +1,7 @@
+package util.enumeration;
+
+public enum PartnerAccessRight 
+{
+    EMPLOYEE,
+    RESERVATIONMANAGER
+}
